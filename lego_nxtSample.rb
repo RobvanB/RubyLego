f10bac6242476d3baa142c226a5f4ad8ffd0657e
Rob vanbrandenburg <robvanb@gmail.com>
2012-08-05 14:02:08 -0400

    Renamed USB1 to ruby-nxtSample.rb Copied USB1 to lego_nxtSample.rb Fixed/changed autodetect_nxt.rb to work when no TTY is created Fixed message in autodetect_nxt to make sure that it didn't say it detected the NXT when it didn't

--- lego_nxtSample.rb
+++ lego_nxtSample.rb
@@ -1,31 +1,27 @@
-class Usb1
+class LegoNxtSample
   $:.unshift File.join(File.dirname(__FILE__),"/lego_nxt/lib")
   $:.unshift File.join(File.dirname(__FILE__),"/ruby-nxt/ruby/lib")
 
-  #gem 'ruby-nxt'  #See readme
-  #gem 'ruby-usb'  #See readme
-  #gem 'libusb'    # Is loaded by lego_nxt
   gem 'serialport'
-  #gem 'lego_nxt'
 
   require 'usb'
   require 'pp'
-  #require 'nxt'
   require 'serialport'
   require 'lego_nxt'
-  #require 'lego_nxt/usb_connection'
 
   #pp USB.devices          #pretty print
-
-  #dev = USB.find_bus(2).find_device(3)
-
-  #nxt =  NXT.new(dev)
 
   conn =  LegoNXT::UsbConnection.new
 
   #Send beep
-  conn.transmit [0x80, 0x03, 0xf4, 0x01, 0xf4, 0x01].pack('C*')
+  #conn.transmit [0x80, 0x03, 0xf4, 0x01, 0xf4, 0x01].pack('C*')
 
+  brick =  LegoNXT::Brick.new(conn)
 
+  brick.run_motor(:a)
+
+  sleep(3)
+
+  brick.stop_motor(:all)
 
 end
---
+++ ruby-nxtSample.rb
@@ -0,0 +1,21 @@
+class RubyNxtSample
+  $:.unshift File.join(File.dirname(__FILE__),"/ruby-nxt/ruby/lib")
+
+  #gem 'ruby-nxt'  #See readme
+  #gem 'ruby-usb'  #See readme
+  #gem 'libusb'    # Is loaded by lego_nxt
+  gem 'serialport'
+  #gem 'lego_nxt'
+
+  require 'usb'
+  require 'pp'
+  require 'nxt'
+  require 'serialport'
+
+  #pp USB.devices          #pretty print
+
+  #dev = USB.find_bus(2).find_device(3)
+
+  nxt =  NXT.new()
+
+end
