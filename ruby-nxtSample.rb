43b7f1cbf98cbd7d6e42b1853eb4e71273bab043
Rob vanbrandenburg <robvanb@gmail.com>
2012-08-09 20:52:20 -0400

    Updated ruby-nxtSample.rb It has a sample to turn the motor and to turn on the Green Led. The nxt.new is still giving an error - will need to debug.

--- ruby-nxtSample.rb
+++ ruby-nxtSample.rb
@@ -1,8 +1,8 @@
 class RubyNxtSample
   $:.unshift File.join(File.dirname(__FILE__),"/ruby-nxt/ruby/lib")
 
-  #gem 'ruby-nxt'  #See readme
-  #gem 'ruby-usb'  #See readme
+  #gem 'ruby-nxt'    #See readme
+  #gem 'ruby-usb'   #See readme
   #gem 'libusb'    # Is loaded by lego_nxt
   gem 'serialport'
   #gem 'lego_nxt'
@@ -12,7 +12,7 @@ class RubyNxtSample
   require 'nxt'
   require 'serialport'
 
-  $DEBUG=true
+  #$DEBUG=true
 
   #pp USB.devices          #pretty print
 
@@ -20,4 +20,31 @@ class RubyNxtSample
 
   nxt =  NXT.new()
 
+  #nxt.sensor(3, "use_ambient_mode")
+
+  #l = Commands::LightSensor.new(nxt)
+  #l.illuminated_mode
+  #l.generate_light
+
+  #nxt.motor_a {|m| m.forward(:degrees => 180)}
+  #nxt.disconnect
+
+  #lproc = Proc.new(light_sensor.generate_light)
+=begin
+  COLORFULL = 0x0D  #NXT 2.0 color sensor in full color mode (color sensor mode)
+  +	COLORRED = 0x0E   #NXT 2.0 color sensor with red light on  (light sensor mode)
+  +	COLORGREEN = 0x0F #NXT 2.0 color sensor with green light on (light sensor mode)
+  +	COLORBLUE = 0x10  #NXT 2.0 color sensor in with blue light on (light sensor mode)
+  +	COLORNONE = 0x11  #NXT 2.0 color sensor in with light off (light sensor mode)
+  +	COLOREXIT =0x12   #NXT 2.0 color sensor internal state  (not sure what this is for yet)
+=end
+
+  #nxt.sensor_light {|l|l.use_illuminated_mode}
+  puts "Turn on Green light"
+  nxt.set_input_mode(0x02, 0x0F, 0x00)    # 0x02 = Port 3 (Light Sensor) , 0x0F = Green, 0x00 = Raw mode
+  #nxt.disconnect
+  sleep(5)
+  puts "Turn off Green Light"
+  nxt.set_input_mode(0x02, 0x11, 0x00)
+  nxt.disconnect
 end
