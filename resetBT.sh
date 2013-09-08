1840202ef3293d794382fa24a1b64acad4cc0057
Rob vanBrandenburg <RobvanB@gmail.com>
2013-09-07 20:47:57 -0400

    Added shell file to initiate BT connection

---
+++ resetBT.sh
@@ -0,0 +1,19 @@
+#Get the adapter ID
+BTDEV=`hcitool dev | awk '{ if( NR == 2 ) print $1 }'`
+
+# Release current connection (if any)
+sudo rfcomm release ${BTDEV}
+
+#Find the address of the device
+#CLIENTADDR=`hcitool scan | awk '{ if( NR == 2 ) print $1 } '`
+# to speed up things we hardcode the address for now
+CLIENTADDR=00:16:53:11:CB:1B
+
+# Set permissions
+sudo chmod 777 /dev/rfcomm0
+
+# Bind the device to the adapter
+sudo rfcomm bind ${BTDEV} ${CLIENTADDR}
+
+echo "Device:"
+ls -l /dev/rfcomm*
